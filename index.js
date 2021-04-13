d44eea6af5b0cce30e10b7607d3d693139d5e0d3
Luicil Fernandes <luicilfernandes@hotmail.com>
2021-04-13 11:11:28 -0300

    Savando arquivos

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const fs = require("fs");
 
-fs.readFile("./txt/arquivo.txt",{encoding: "utf-8"},(err, data) =>{
-    if(!err){
-        console.log(data);
-    } else {
-        console.log("Erro: " + err);
-    }
+// fs.readFile("./txt/arquivo.txt",{encoding: "utf-8"},(err, data) =>{
+//     if(!err){
+//         console.log(data);
+//     } else {
+//         console.log("Erro: " + err);
+//     }
     
-});
+// });
+
+fs.writeFile("./txt/teste.txt","Teste de gravação !",{encoding: "utf8"},(err) =>{
+    if(err){
+        console.log("Erro: " + err);
+    } else {
+        console.log("Arquivo salvo com sucesso !");
+    }
+})
